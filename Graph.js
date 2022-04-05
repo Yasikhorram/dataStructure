3621a1eac5f458f9bcba7b0b07200a1dec797924
Yasikhorram <khorramyasaman@gmail.com>
2022-04-05 17:47:47 -0500

    remove vertex method

--- Graph.js
+++ Graph.js
@@ -14,4 +14,22 @@ class Graph {
     this.adjacencyList[v1].push(v2);
     this.adjacencyList[v2].push(v1);
   }
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+      (vertex) => vertex !== v2
+    );
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+      (vertex) => vertex !== v1
+    );
+  }
 }
+
+let g = new Graph();
+g.addVertex("Tokyo");
+g.addVertex("Tehran");
+g.addVertex("Austin");
+g.addEdge("Tokyo", "Austin");
+console.log("before remove", g);
+
+g.removeEdge("Tokyo", "Austin");
+console.log(g);
