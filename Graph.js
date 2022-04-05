22693686f17f05d4dfdfac21a45606c734f8a2cf
Yasikhorram <khorramyasaman@gmail.com>
2022-04-05 16:48:01 -0500

    implement add vertex method

--- Graph.js
+++ Graph.js
@@ -2,4 +2,8 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+  addVertex(vertex) {
+    //In this way if we have already have same key it will overwrite it
+    this.adjacencyList[vertex] = [];
+  }
 }
