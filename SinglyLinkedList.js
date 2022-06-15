df130137f7936576f3a6a133a77fc75048abbc76
Yasikhorram <khorramyasaman@gmail.com>
2022-06-14 19:13:38 -0500

    push method

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -6,9 +6,35 @@ class Node {
 }
 
 class SinglyLinkedList {
-  constructor() {}
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
-let firstNode = new Node(1);
-firstNode.next = new Node(2);
-console.log(firstNode);
+// let firstNode = new Node(1);
+// firstNode.next = new Node(2);
+// console.log(firstNode);
+
+let newSing = new SinglyLinkedList();
+
+newSing.push("test");
+newSing.push("test2");
+newSing.push("test3");
+newSing.push("test4");
+newSing.push("test5");
+
+console.log(newSing);
